4b725a9a927554a481ca8efad76ebd4796140073
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-03 01:33:21 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
+import { Switch, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import AppBar from './Components/AppBar/AppBar';
+import HomePage from './views/HomePage/HomePage';
+import MoviesPage from './views/MoviesPage/MoviesPage';
+import NotFoundView from './views/NotFoundView/NotFoundView';
+import MovieDetailsPage from './views/MovieDetailsPage/MovieDetailsPage';
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ToastContainer autoclose={3000} />
+      <AppBar />
+
+      <Switch>
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+
+        <Route path="/movies" exact>
+          <MoviesPage />
+        </Route>
+
+        <Route path="/movies/:movieId">
+          <MovieDetailsPage />
+        </Route>
+
+        <Route>
+          <NotFoundView />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
