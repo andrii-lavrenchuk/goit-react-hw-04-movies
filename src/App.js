e906adbca60b977800bec9fe0214a8fe40fc8917
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-02 14:08:03 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-// import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import ImageGallery from './Components/ImageGallery/ImageGallery';
-
 export default function App() {
-  return (
-    <div>
-      <ToastContainer autoClose={3000} />
-      <ImageGallery />
-    </div>
-  );
+  return <div></div>;
 }
