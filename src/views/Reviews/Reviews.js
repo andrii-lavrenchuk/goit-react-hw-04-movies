1e03d871dd0f4b9f840d522885e889a3057cfdac
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-05 23:42:40 +0200

    Update Reviews.js

---
+++ src/views/Reviews/Reviews.js
@@ -0,0 +1,26 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import * as apiService from '../../services/films-api';
+
+export default function Reviews() {
+  const { movieId } = useParams();
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    apiService.getMovieReviews(movieId).then(({ results }) => {
+      setReviews(results);
+    });
+  }, [movieId]);
+
+  return (
+    <ul>
+      {reviews.map(review => (
+        <li key={review.id}>
+          <h2>Author: {review.author}</h2>
+          <p>{review.content}</p>
+        </li>
+      ))}
+    </ul>
+  );
+}
