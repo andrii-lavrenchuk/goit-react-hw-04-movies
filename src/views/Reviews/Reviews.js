a444f5bb5c0b4c659fe994f791b56a6939780d92
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-09 18:35:25 +0200

    Update Reviews.js

--- src/views/Reviews/Reviews.js
+++ src/views/Reviews/Reviews.js
@@ -3,42 +3,66 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { toast } from 'react-toastify';
 import s from './Reviews.module.css';
-
+import Loader from '../../Components/Loader/Loader';
 import * as apiService from '../../services/films-api';
+import ErrorView from '../../Components/ErrorView/ErrorView';
+
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 
 export default function Reviews() {
   const { movieId } = useParams();
   const [reviews, setReviews] = useState([]);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
+
   useEffect(() => {
-    apiService.getMovieReviews(movieId).then(({ results }) => {
-      if (results.length === 0) {
-        toast.error('There are no reviews for this movie ');
-        return;
-      }
-      setReviews(results);
-    });
+    apiService
+      .getMovieReviews(movieId)
+      .then(({ results }) => {
+        if (results.length === 0) {
+          toast.error('There are no reviews for this movie ');
+          return;
+        }
+        setReviews(results);
+        setStatus(Status.RESOLVED);
+      })
+      .catch(error => {
+        setError('Something went wrong. Try again.');
+        setStatus(Status.REJECTED);
+      });
   }, [movieId]);
 
   return (
-    <ul>
-      {reviews.map(review => (
-        <li key={review.id} className={s.review}>
-          <h2 className={s.reviewAuthor}>Author: {review.author}</h2>
-          <p className={s.reviewContent}>
-            <ShowMore
-              text={review.content}
-              length={170}
-              showMoreLabel="Show more"
-              showLessLabel="Show less"
-              style={{
-                cursor: 'pointer',
-                color: 'green',
-                fontWeight: 'bold',
-              }}
-            />
-          </p>
-        </li>
-      ))}
-    </ul>
+    <>
+      {status === Status.PENDING && <Loader />}
+      {status === Status.REJECTED && <ErrorView message={error} />}
+      {status === Status.RESOLVED && (
+        <ul>
+          {reviews.map(review => (
+            <li key={review.id} className={s.review}>
+              <h2 className={s.reviewAuthor}>Author: {review.author}</h2>
+              <p className={s.reviewContent}>
+                <ShowMore
+                  text={review.content}
+                  length={170}
+                  showMoreLabel="Show more"
+                  showLessLabel="Show less"
+                  style={{
+                    cursor: 'pointer',
+                    color: 'green',
+                    fontWeight: 'bold',
+                  }}
+                />
+              </p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
