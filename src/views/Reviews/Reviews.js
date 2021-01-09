afec1a4ddb6e331581af7f51d5e95454e1424f32
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-09 17:12:23 +0200

    Update Reviews.js

--- src/views/Reviews/Reviews.js
+++ src/views/Reviews/Reviews.js
@@ -1,5 +1,8 @@
+import ShowMore from 'react-simple-show-more';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import { toast } from 'react-toastify';
+import s from './Reviews.module.css';
 
 import * as apiService from '../../services/films-api';
 
@@ -8,18 +11,32 @@ export default function Reviews() {
   const [reviews, setReviews] = useState([]);
   useEffect(() => {
     apiService.getMovieReviews(movieId).then(({ results }) => {
+      if (results.length === 0) {
+        toast.error('There are no reviews for this movie ');
+        return;
+      }
       setReviews(results);
     });
   }, [movieId]);
 
-  // console.log(reviews);
-
   return (
     <ul>
       {reviews.map(review => (
-        <li key={review.id}>
-          <h2>Author: {review.author}</h2>
-          <p>{review.content}</p>
+        <li key={review.id} className={s.review}>
+          <h2 className={s.reviewAuthor}>Author: {review.author}</h2>
+          <p className={s.reviewContent}>
+            <ShowMore
+              text={review.content}
+              length={170}
+              showMoreLabel="Show more"
+              showLessLabel="Show less"
+              style={{
+                cursor: 'pointer',
+                color: 'green',
+                fontWeight: 'bold',
+              }}
+            />
+          </p>
         </li>
       ))}
     </ul>
