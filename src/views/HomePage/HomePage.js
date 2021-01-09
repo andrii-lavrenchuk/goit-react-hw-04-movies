18b509204c67a86f0bbb92661bcd2d7e87eb4fd3
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-09 18:35:14 +0200

    Update HomePage.js

--- src/views/HomePage/HomePage.js
+++ src/views/HomePage/HomePage.js
@@ -2,34 +2,47 @@ import { useState, useEffect } from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
 import * as apiService from '../../services/films-api';
 import s from './HomePage.module.css';
+import Loader from '../../Components/Loader/Loader';
+import ErrorView from '../../Components/ErrorView/ErrorView';
+import notFoundImg from '../../img/notFound.png';
 
+const Status = {
+  IDLE: 'idle',
+  PENDING: 'pending',
+  RESOLVED: 'resolved',
+  REJECTED: 'rejected',
+};
 export default function HomePage() {
   const { url } = useRouteMatch();
 
   const [movies, setMovies] = useState(null);
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState(Status.IDLE);
 
   useEffect(() => {
-    // setStatus(Status.PENDING);
+    setStatus(Status.PENDING);
     apiService
       .getTrendingFilms()
       .then(({ results }) => {
         setMovies(results);
-        // setTotalPage(total_pages);
-        // setStatus(Status.RESOLVED);
+        setStatus(Status.RESOLVED);
       })
       .catch(error => {
-        console.log(error);
-        // setError('Something went wrong. Try again.');
-        // setStatus(Status.REJECTED);
+        setError('Something went wrong. Try again.');
+        setStatus(Status.REJECTED);
       });
   }, []);
 
   return (
     <>
       <h2 className={s.title}>Trending today</h2>
-      <ul className={s.movieGallery}>
-        {movies &&
-          movies.map(movie => (
+      {status === Status.PENDING && <Loader />}
+
+      {status === Status.REJECTED && <ErrorView message={error.message} />}
+
+      {status === Status.RESOLVED && (
+        <ul className={s.movieGallery}>
+          {movies.map(movie => (
             <li key={movie.id} className={s.movieGalleryItem}>
               <Link to={`${url}movies/${movie.id}`}>
                 <img
@@ -37,7 +50,7 @@ export default function HomePage() {
                   src={
                     movie.poster_path
                       ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-                      : 'noImageFound'
+                      : notFoundImg
                   }
                   alt={movie.title}
                 />
@@ -45,7 +58,8 @@ export default function HomePage() {
               </Link>
             </li>
           ))}
-      </ul>
+        </ul>
+      )}
     </>
   );
 }
