c3ab084e1adddb76ee1c93aeba2326bd067ccb66
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-09 18:35:22 +0200

    Update NotFoundView.js

--- src/views/NotFoundView/NotFoundView.js
+++ src/views/NotFoundView/NotFoundView.js
@@ -1,3 +1,11 @@
+import errorImg from '../../img/error.jpg';
+import s from './NotFoundView.module.css';
+
 export default function NotFoundView() {
-  return <h1>404 Page not found</h1>;
+  return (
+    <div className={s.main}>
+      <img className={s.img} src={errorImg} width="650" alt="Page not found" />
+      <h1 className={s.title}>404 Page not found</h1>
+    </div>
+  );
 }
