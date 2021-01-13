90642472aa35e920d95683f722ad999cf3f0d350
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2021-01-13 21:26:38 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
+import { lazy, Suspense } from 'react';
+import Loader from './Components/Loader/Loader';
+
 import { Switch, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import AppBar from './Components/AppBar/AppBar';
-import HomePage from './views/HomePage/HomePage';
-import MoviesPage from './views/MoviesPage/MoviesPage';
-import NotFoundView from './views/NotFoundView/NotFoundView';
-import MovieDetailsPage from './views/MovieDetailsPage/MovieDetailsPage';
+
+const HomePage = lazy(() =>
+  import('./views/HomePage/HomePage' /* webpackChunkName: "home-page" */),
+);
+
+const MoviesPage = lazy(() =>
+  import('./views/MoviesPage/MoviesPage' /* webpackChunkName: "movies-page" */),
+);
+
+const NotFoundView = lazy(() =>
+  import(
+    './views/NotFoundView/NotFoundView' /* webpackChunkName: "not-found-view" */
+  ),
+);
+
+const MovieDetailsPage = lazy(() =>
+  import(
+    './views/MovieDetailsPage/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */
+  ),
+);
 
 export default function App() {
   return (
     <div>
       <ToastContainer autoClose={2000} />
       <AppBar />
+      <Suspense fallback={<Loader />}>
+        <Switch>
+          <Route path="/" exact>
+            <HomePage />
+          </Route>
 
-      <Switch>
-        <Route path="/" exact>
-          <HomePage />
-        </Route>
+          <Route path="/movies" exact>
+            <MoviesPage />
+          </Route>
 
-        <Route path="/movies" exact>
-          <MoviesPage />
-        </Route>
+          <Route path="/movies/:movieId">
+            <MovieDetailsPage />
+          </Route>
 
-        <Route path="/movies/:movieId">
-          <MovieDetailsPage />
-        </Route>
-
-        <Route>
-          <NotFoundView />
-        </Route>
-      </Switch>
+          <Route>
+            <NotFoundView />
+          </Route>
+        </Switch>
+      </Suspense>
     </div>
   );
 }
